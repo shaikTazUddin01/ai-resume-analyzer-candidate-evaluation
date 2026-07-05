2bc8a155a4dfe593e63ef67c55754c72f297952a
shaikTazUddin01 <tazahmedsoft@gmail.com>
2026-07-05 23:34:40 +0600

    fix: updated upload path to tmp for vercel

--- backend/src/utils/extractResumeText.js
+++ backend/src/utils/extractResumeText.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const uploadDir = path.join('/tmp', 'resumes');
 
 if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
+    fs.mkdirSync(uploadDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
