38855aedb3b54bdc4e08c273f0df224dbdeb8a43
shaikTazUddin01 <tazahmedsoft@gmail.com>
2026-07-05 23:31:28 +0600

    update

--- backend/src/utils/extractResumeText.js
+++ backend/src/utils/extractResumeText.js
@@ -2,7 +2,8 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
-const uploadDir = "uploads/resumes";
+// const uploadDir = "uploads/resumes";
+const uploadDir = path.join('/tmp', 'resumes');
 
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
